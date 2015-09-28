2316b1161c7092d2c9aa974c2fc9cd49f8966147
fredcallaway <fredc@llaway.com>
2015-09-28 15:05:41 -0400

    Final commit before submitting to CS 4740

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # BigramsCompleteMe
-
-BigramsCompleteMe is a sublime text plugin that sorts autocompletion results by their probability given the previous word. The plugin is designed to be used for editing text files, the main feature being improved "insert_best_completion" accuracy. In many cases, you can repeat an existing phrase such as "sublime text" with just the first letters of each word (e.g. `s`, `space`, `tab`, `t`, `space`, `tab`).
+BigramsCompleteMe is a sublime text plugin that sorts autocompletion results by their probability given the previous word. In many cases, you can repeat an existing phrase such as "sublime text" with just the first letters of each word (e.g. `s`, `space`, `tab`, `t`, `space`, `tab`).
 
 ![Example usage](https://i.gyazo.com/05161b7d00b08d9f2427e3c20d655182.gif)
 
-# Installation
+## Installation
     
     cd '~Library/Application Support/Sublime Text 3/Packages/'
     git clone https://github.com/fredcallaway/BigramsCompleteMe.git
+
+## Caveats
+At present, BigramsCompleteMe does not work with sublime's fuzzy autocomplete feature. One can implement fuzzy autocompletion by constructing a regular expression of a character list joined by `.*` e.g. `b.*c.*m` for BigramsCompleteMe. However, sublime text's autocompletion API does not respect the order of autocompletion suggestions when fuzzy matching. This is likely because sublime text trumps the plugin's sorting with its own fuzzy match sorting.
+
+## Future
+
+- Fuzzy completion by remapping tab to `insert_best_bigram_completion`. This will involve a fair ammount of code due to cycle through options, etc...
+- Pretrained bigram models in addition to current-file bigrams.
--- completion.py
+++ completion.py
@@ -1,5 +1,6 @@
-from .bigrams import BigramModel
+from .bigrams import Distribution
 
+from collections import Counter
 import re
 import sublime_plugin
 import sublime
@@ -12,25 +13,36 @@ class BigramsCompleteMe(sublime_plugin.EventListener):
             # only operate when editing a text field
             return
 
-        # exclude the current prefix from the bigram model
         prefix_region = view.word(location)
         previous_word = view.substr(view.word(prefix_region.begin() - 1))
-        before_region = sublime.Region(0, prefix_region.begin())
-        after_region = sublime.Region(prefix_region.end(), view.size())
-        all_text = view.substr(before_region) + view.substr(after_region)
-
-        tokens = re.sub(r'[^\w\s]', '', all_text).split()
 
         if prefix:
             completions = view.extract_completions(prefix)
         else:
+            # exclude the current prefix from the bigram model
+            all_text = view.substr(sublime.Region(0, view.size()))
+
+            tokens = re.sub(r'[^\w\s\']', ' ', all_text).split()
             completions = list(set(tokens)) 
 
-        model = BigramModel(tokens, smoothing=False, track_rare=False)
+        following_words = get_words_following(view, previous_word)
+        # don't count the prefix that you're currently typing
+        if following_words[prefix]:
+            following_words[prefix] -= 1
+
+        distribution = Distribution(following_words)
 
         def probability(word):
-            return model.probability(previous_word, word)
+            return distribution.probability(word)
 
         completions.sort(key=lambda c: - probability(c))
         matches = [('%s\t%2.2f' % (c, probability(c)), c) for c in completions]
         return matches
+
+
+def get_words_following(view, word):
+    pattern = r'\W%s\s' % word
+    matches = view.find_all(pattern, sublime.IGNORECASE)
+    followers = [view.substr(view.word(m.end() + 1))
+                 for m in matches]
+    return Counter(followers)
